75c2e34d59e693bb33e8d0192fb792a239c6852d
Vivechan B <52950173+vivechanchanny@users.noreply.github.com>
2024-03-15 10:26:23 +0530

    Update main.tf

--- modules/alb_dns/main.tf
+++ modules/alb_dns/main.tf
@@ -6,16 +6,36 @@ resource "aws_lb" "alb" {
   subnets            = var.subnets
 }
 
+# resource "aws_acm_certificate" "tempdns" {
+#   domain_name       = "archerymontessori.com"
+#   validation_method = "DNS"
+# }
+
 resource "aws_lb_listener" "http_listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 80
   protocol          = "HTTP"
+  # ssl_policy        = "ELBSecurityPolicy-2016-08"
+  # certificate_arn   = aws_acm_certificate.tempdns.arn
 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.target_group.arn
   }
 }
+
+# resource "aws_lb_listener" "https" {
+#   load_balancer_arn = aws_lb.alb.arn
+#   port              = "443"
+#   protocol          = "HTTPS"
+#   # ssl_policy        = "ELBSecurityPolicy-2016-08"
+#   # certificate_arn   = aws_acm_certificate.tempdns.arn
+  
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.target_group.arn
+#   }
+# }
 
 resource "aws_lb_target_group" "target_group" {
   name        = var.target_group_name
